10db7b64715322e4d3b84cba9fbc245025b6273f
Bruno <brunocleao99@gmail.com>
2024-05-05 19:18:24 -0300

    Guilherme - Adicionando o sldie show no index

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,21 @@
 // Declarando variaveis
+let imagens = ['/src/assets/img/bike1.jpg', '/src/assets/img/bike2.jpg', '/src/assets/img/bike3.jpg', '/src/assets/img/bike4.jpg']
+let index = 0;
+let time = 4000;
 
 function mudarCor(cor){
     var body = document.body;
 
     body.style.backgroundColor = cor;
 }
+
+function slideShow(){
+    document.getElementById('imgSlideshow').src = imagens[index];
+    index++;
+
+    if(index == imagens.length){
+        index = 0;
+    }
+    setTimeout('slideShow()', time);
+}
+slideShow();
